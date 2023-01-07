0ebde068ddad21270bba58c6aefcf935c3fb9bc6
nikifor <nik.slepcov1771@gmail.com>
2023-01-08 01:55:28 +0300

    creating live currency

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { useEffect, useRef, useState } from 'react';
 function App(props) {
   const [fromCurrency , setFromCurrency] = useState('UAH');
   const [toCurrency, setToCurrency] = useState('USD');
+  const [toEUR, setToEUR] = useState('EUR');
+  const [toUSD, setToUSD] = useState('USD');
   const [fromPrice, setFromPrice] = useState(0);                  // <---- Creating state for the component
   const [toPrice, setToPrice] = useState(0);
 
@@ -41,16 +43,25 @@ function App(props) {
     setFromPrice(result.toFixed(3));
     setToPrice(value);
   };
-  const onChangeUSD = (value) => {
-    const price = value / ratesRef.current[toCurrency];
-    const result = price * ratesRef.current[fromCurrency];
-    setFromPrice(result.toFixed(3));
-    setToPrice(value);
+  const onChangeUSD = () => {
+    const price = 1 / ratesRef.current[toUSD];
+    setToUSD(price.toFixed(3));
+  };
+  const onChangeEUR = () => {
+    const price = 1 / ratesRef.current[toEUR];
+    setToEUR(price.toFixed(3));
   };
   return (
     <div className="converter_container">
+      <h1 className="currency_title">
+            Currency Converter
+        </h1>
+        <h1 className="currency_live-title">
+            Current exchange rates
+        </h1>
       <header className="converter_header">
-        <Live value={fromPrice} currency_usd={toCurrency} currency_uah={onChangeUSD} currency_eur={"EUR"} onChangeCurrency={1}/>
+        <Live value={onChangeUSD} currency_value={"USD"} currency_uah={"UAH"} onChangeCurrency={1}/>
+        <Live value={onChangeEUR} currency_value={"EUR"} currency_uah={"UAH"} onChangeCurrency={1}/>
       </header>
       <Converter value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice} />
       <Converter value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice}/> 
--- src/components/LiveCurrency/Live.jsx
+++ src/components/LiveCurrency/Live.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 
-export const Live = ({ value, currency_usd, currency_uah, currency_eur, onChangeCurrency, onChangeValue, onChangeUSD }) => (
+export const Live = ({ value, currency_value, currency_uah,onChangeCurrency }) => (
     <div className="currency_live">
-        <h1 className="currency_title">
-            Currency Converter
-        </h1>
-        <h1 className="currency_live-title">
-            Current exchange rates
-        </h1>
-        <div className="currency_live-USD">
-            {onChangeCurrency} {currency_usd} = {value} {currency_uah}
-        </div>
-        <div className="currency_live-EUR">
-            1 {currency_eur} = {value} {currency_uah}
+        <div className="currency_live-course">
+            {onChangeCurrency} {currency_value} = {value} {currency_uah}
         </div>
     </div>
 )
